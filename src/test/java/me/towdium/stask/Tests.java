0befd115900b9896deb07ef26426a0f2e6e59f50
towdium <towdium@gmail.com>
2019-08-01 23:28:51 +0100

    Remove outdated test

--- src/test/java/me/towdium/stask/Tests.java
+++ src/test/java/me/towdium/stask/Tests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import static java.lang.System.out;
-
 /**
  * Author: Towdium
  * Date: 06/03/19
@@ -14,11 +12,5 @@ import static java.lang.System.out;
 public class Tests {
     @Test
     public void test() {
-        double a = 1. / 64 / 3;
-        double b = 0;
-        for (int i = 0; i < 64 * 3; i++) {
-            b += a;
-        }
-        out.println(b >= 1);
     }
 }
